c6f0a86cd73c9c803f50216b65d448d10660ae24
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-05 22:01:01 +0530

    kmk

---
+++ basics1.py
@@ -0,0 +1,20 @@
+print("hello world") #hello world printing
+print("hello\nI am yash kumar mishra\n from bareilly") #print in new line
+
+"""
+print("njnknk")
+print("kjjiokji")
+
+"""
+print("hello\nI am \"yash\" kumar mishra\n from bareilly") # " " print karna
+
+print("jj",6,8787979, "ojo", sep="~", end="989\n")
+print("yash")
+
+a=23
+print(a)
+print(type(a))
+ 
+list1=[8,3,"oke"]
+print(list1)
+
