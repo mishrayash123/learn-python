67bbfaa7bc1bd5adbdd0a10859ae4b281087fe3c
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-06 12:49:07 +0530

    kj

---
+++ basic2.py
@@ -0,0 +1,21 @@
+# typecasting
+a="1"
+b="4"
+print(int(a)+int(b))
+
+# 1. explicit conversion(hum kahte hain karne ko)
+# 2. implicit conversion(python khud karta hai)
+
+# input
+
+# a=input()
+# a=input("enter your name : ")
+# print("my name is",a)
+
+x=input("enter first number : ")
+y=input("enter second number : ")
+
+print(x+y)
+
+print(int(x)+int(y))
+
