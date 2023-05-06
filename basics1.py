376dcf5caaab1f8340aa303194667ecc05edb752
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-06 11:46:12 +0530

    datatypesand operators

--- basics1.py
+++ basics1.py
@@ -10,7 +10,8 @@ print("hello\nI am \"yash\" kumar mishra\n from bareilly") # " " print karna
 
 print("jj",6,8787979, "ojo", sep="~", end="989\n")
 print("yash")
-
+ 
+ # datatypes
 a=23
 print(a)
 print(type(a))
@@ -18,3 +19,14 @@ print(type(a))
 list1=[8,3,"oke"]
 print(list1)
 
+tuple1 = (("parrot","sparrow"),("lion","tiger"))  # immutable (non changable)
+print(tuple1)
+
+dict1={"name":"yash","age":20} #maped data (dictionary)
+print(dict1)
+
+# operators
+
+print(15//6) #floor division operator
+
+print(5**3) #power
