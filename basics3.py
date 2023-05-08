04a04f1d60b721c6c84b7b589b5e21027b6af12c
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-08 22:07:24 +0530

    jkk

--- basics3.py
+++ basics3.py
@@ -8,4 +8,85 @@ print(names[:])
 print(names[0:-3]) # print(names[0:len(names)-3])
 print(names[-1:4]) # print(names[len(names)-1:4]) 10:6 nonsence
 
+#check charecter present in string or not
+txt = "The best things in life are free!"
+print("free" in txt)
 
+#Check if "expensive" is NOT present in the following text
+print("expensive" not in txt)
+
+#use of if
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#to uper
+print(txt.upper())
+
+#to lower
+print(txt.lower())
+
+# remove space
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+# replace string
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# break when space comes
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+# formating of string
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+# string methods
+
+# capitalize()	Converts the first character to upper case
+# casefold()	Converts string into lower case
+# center()	Returns a centered string
+# count()	Returns the number of times a specified value occurs in a string
+# encode()	Returns an encoded version of the string
+# endswith()	Returns true if the string ends with the specified value
+# expandtabs()	Sets the tab size of the string
+# find()	Searches the string for a specified value and returns the position of where it was found
+# format()	Formats specified values in a string
+# format_map()	Formats specified values in a string
+# index()	Searches the string for a specified value and returns the position of where it was found
+# isalnum()	Returns True if all characters in the string are alphanumeric
+# isalpha()	Returns True if all characters in the string are in the alphabet
+# isdecimal()	Returns True if all characters in the string are decimals
+# isdigit()	Returns True if all characters in the string are digits
+# isidentifier()	Returns True if the string is an identifier
+# islower()	Returns True if all characters in the string are lower case
+# isnumeric()	Returns True if all characters in the string are numeric
+# isprintable()	Returns True if all characters in the string are printable
+# isspace()	Returns True if all characters in the string are whitespaces
+# istitle()	Returns True if the string follows the rules of a title
+# isupper()	Returns True if all characters in the string are upper case
+# join()	Joins the elements of an iterable to the end of the string
+# ljust()	Returns a left justified version of the string
+# lower()	Converts a string into lower case
+# lstrip()	Returns a left trim version of the string
+# maketrans()	Returns a translation table to be used in translations
+# partition()	Returns a tuple where the string is parted into three parts
+# replace()	Returns a string where a specified value is replaced with a specified value
+# rfind()	Searches the string for a specified value and returns the last position of where it was found
+# rindex()	Searches the string for a specified value and returns the last position of where it was found
+# rjust()	Returns a right justified version of the string
+# rpartition()	Returns a tuple where the string is parted into three parts
+# rsplit()	Splits the string at the specified separator, and returns a list
+# rstrip()	Returns a right trim version of the string
+# split()	Splits the string at the specified separator, and returns a list
+# splitlines()	Splits the string at line breaks and returns a list
+# startswith()	Returns true if the string starts with the specified value
+# strip()	Returns a trimmed version of the string
+# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+# title()	Converts the first character of each word to upper case
+# translate()	Returns a translated string
+# upper()	Converts a string into upper case
+# zfill()	Fills the string with a specified number of 0 values at the beginning
