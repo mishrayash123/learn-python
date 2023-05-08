b271b7fbe1cde86967e6e14f2929e9d5777ff58d
yash kumar mishra <mishrayash3778@gmail.com>
2023-05-08 13:40:10 +0530

    jk

--- basic2.py
+++ basic2.py
@@ -8,9 +8,9 @@ print(int(a)+int(b))
 
 # input
 
-# a=input()
-# a=input("enter your name : ")
-# print("my name is",a)
+a=input()
+a=input("enter your name : ")
+print("my name is",a)
 
 x=input("enter first number : ")
 y=input("enter second number : ")
@@ -18,4 +18,19 @@ y=input("enter second number : ")
 print(x+y)
 
 print(int(x)+int(y))
+ 
+ # strings
 
+name = "yash "
+middle= "kumar "
+last= "mishra"
+
+print("my name is " +name+middle+last)
+
+sty=""" jhwhswj
+idiudiou
+iduioeudo"""
+
+print(sty)
+
+print(name[0])
